0181d299375d8d454b3b8f19af59cd32a200f5aa
Risto Pavlov <ristopavlov@gmail.com>
2019-07-01 20:12:55 +0200

    added code from class 1

---
+++ g2/Class1/SEDC.G2.ADONET/PizzaSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.G2.ADONET
+{
+    public class PizzaSize
+    {
+        public int SizeId { get; set; }
+        public int PizzaId { get; set; }
+        public decimal Price { get; set; }
+    }
+}

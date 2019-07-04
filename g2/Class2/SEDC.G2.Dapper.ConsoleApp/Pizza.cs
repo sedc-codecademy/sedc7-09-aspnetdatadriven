6522d4e0d69da95f8d7c0f62bb1602af8300b66b
Risto Pavlov <ristopavlov@gmail.com>
2019-07-04 23:17:10 +0200

    added code from class 2

---
+++ g2/Class2/SEDC.G2.Dapper.ConsoleApp/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.G2.Dapper.ConsoleApp
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

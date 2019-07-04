aec4abb0b0f7c41b4c9e66808dea87e26da77d1c
Risto Panchevski <risto.panchevski@outlook.com>
2019-07-04 18:59:20 +0200

    Dapper demo

---
+++ g3/Dapper/CompanyApp-Dapper/CompanyApp-Dapper/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿namespace CompanyApp_Dapper.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Company(int id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+
+        public Company()
+        {
+            
+        }
+    }
+}
---
+++ g3/Dapper/CompanyApp-Dapper/CompanyApp-Dapper/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using CompanyApp_Dapper.Models;
+using Dapper;
+
+namespace CompanyApp_Dapper
+{
+    class Program
+    {
+        private static string _connectionString = "Server=.;Database=SEDCCompanyDb-Dapper;Trusted_Connection=True";
+
+        static void Main(string[] args)
+        {
+            var employeeCount = CountEmployees();
+            Console.WriteLine($"No. of employees: {employeeCount}");
+
+            var departments = GetDepartments();
+            foreach (var department in departments)
+            {
+                Console.WriteLine($"{department.Id}: {department.Name}");
+            }
+
+            var filter = Console.ReadLine();
+            var companies = CallSp(filter);
+            foreach (var company in companies)
+            {
+               Console.WriteLine($"{company.Id}: {company.Name} [{company.Address}]"); 
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int CountEmployees()
+        {
+            int count;
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                var sql = "Select Count(*) From Employees";
+
+                count = con.ExecuteScalar<int>(sql);
+            }
+
+            return count;
+        }
+
+        private static List<Department> GetDepartments()
+        {
+            List<Department> departments;
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                var sql = "Select * From Departments";
+
+                departments = con.Query<Department>(sql).ToList();
+            }
+
+            return departments;
+        }
+
+        private static List<Company> CallSp(string filter)
+        {
+            List<Company> companies;
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@companyName", filter);
+
+                companies = con.Query<Company>("GetCompanies", parameters, commandType: CommandType.StoredProcedure)
+                    .ToList();
+            }
+
+            return companies;
+        }
+    }
+}

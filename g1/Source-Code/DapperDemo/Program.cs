1b97f9e478e57e29718047418c429fb10b5d4221
xhevo <dzevat.ibraimi@navetti.com>
2019-07-04 18:23:10 +0200

    added todo model

--- g1/Source-Code/DapperDemo/Program.cs
+++ g1/Source-Code/DapperDemo/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using da
+using Dapper;
 
 namespace DapperDemo
 {
@@ -6,7 +10,16 @@ namespace DapperDemo
     {
         static void Main(string[] args)
         {
-
+            using (var connection = new SqlConnection(""))
+            {
+                connection.Open();
+               IEnumerable<dynamic> todos = 
+                    connection.Query<Todo>("select * from Todos");
+                foreach (dynamic todo in todos)
+                {
+                    todo.ClimbTheMountain();
+                }
+            }
             Console.WriteLine("Hello World!");
         }
     }
---
+++ g1/Source-Code/DapperDemo/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DapperDemo
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public string UserId { get; set; }
+    }
+}

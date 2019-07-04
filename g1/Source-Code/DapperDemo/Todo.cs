f7f3cad624c885c30b3338d05a7526707bb761d3
xhevo <dzevat.ibraimi@navetti.com>
2019-07-04 19:56:34 +0200

    added dd3 finiito

--- g1/Source-Code/DapperDemo/Todo.cs
+++ g1/Source-Code/DapperDemo/Todo.cs
@@ -7,6 +7,7 @@ namespace DapperDemo
         public int Id { get; set; }
         public DateTime? DueDate { get; set; }
         public string Description { get; set; }
+        public string Title { get; set; }
         public bool IsCompleted { get; set; }
         public string UserId { get; set; }
     }

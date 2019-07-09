bf84ae74a8daad046ca882c12f894e31ec9edcf9
xhevo <dzevat.ibraimi@navetti.com>
2019-07-09 19:03:06 +0200

    added api method

---
+++ g1/Source-Code/Sedc.Todo05Solution.WebApp/Controllers/Api/TodoApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sedc.Todo03Solution.Entities;
+using Sedc.Todo03Solution.Repositories.Interfaces;
+using System.Linq;
+
+namespace Sedc.Todo05Solution.WebApp.Controllers.Api
+{
+    [Route("/api")]
+    public class TodoApiController: Controller
+    {
+        private readonly IGenericRepository<Todo> todosRepo;
+
+        public TodoApiController(IGenericRepository<Todo> todosRepo)
+        {
+            this.todosRepo = todosRepo;
+        }
+
+        [HttpGet("todos")]
+        public IActionResult GetTodos()
+        {
+            var results = todosRepo.GetAll().ToList();
+            return new JsonResult(results);
+        }
+    }
+}
